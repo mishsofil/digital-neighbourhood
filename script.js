1dfa4ccf7eec18ff62080b6bcc8e8b3ce40ce864
mishsofil <mishasofil@gmail.com>
2025-11-26 00:46:38 +0000

    Simple vers1

---
+++ script.js
@@ -0,0 +1,24 @@
+let label1 = document.getElementById("label1")
+let label2 = document.getElementById("label2")
+let label3 = document.getElementById("label3")
+
+function openLabel1(){
+    label1.classList.add("open-label");
+}
+function closeLabel1(){
+    label1.classList.remove("open-label");
+}
+
+function openLabel2(){
+    label2.classList.add("open-label");
+}
+function closeLabel2(){
+    label2.classList.remove("open-label");
+}
+
+function openLabel3(){
+    label3.classList.add("open-label");
+}
+function closeLabel3(){
+    label3.classList.remove("open-label");
+}
